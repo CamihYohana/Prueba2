8b6fd49220c7bb75dadc396f99102791a4660cd3
camila badillo <camihyohana@MacBook-Air-de-camila.local>
2018-07-26 16:30:34 -0400

    modificaciones en los 3 archivos, funcionando las delegaciones y conacion de estructura de twit

--- interaction.js
+++ interaction.js
@@ -3,22 +3,53 @@ $(function () {
         var texto = $('#texto-twit').val();
         var nombre = $('#name').val();
 
+        // var tweet = crearMiTweet(img, nombre, texto)
+
         var nuevo = $('.twit').eq(0).clone();
-        nuevo.appendTo('.twits');
-        nuevo.find('.profile_name h2').text(nombre);
+        // tweet.appendTo('#twits');
+        nuevo.appendTo('#twits');
+        nuevo.find('.profile_name h2').val(nombre);
         nuevo.find('.twit__text').text(texto);
 
-        $(this).find('input').val('');
+        $('#texto-twit').val('');
         e.preventDefault();
         e.stopPropagation();
     });
-    $('.twit__like').click(function (event) {
+
+    $('#twits').on("click", ".twit__like", function () {
         event.stopPropagation();
         event.preventDefault();
         $(this).toggleClass('twit__like--red');
     });
-    $('#twit__delete').click(function (e) {
-        $(this).parent().fadeOut();
+
+    $('#twits').on("click", ".twit__delete", function () {
+        $(this).parent().fadeOut(function () {
+            $(this).remove();
+        });
     });
 
 });
+
+// function crearMiTweet(img, nombre, texto) {
+//     var html = '<li class="twit">';
+
+//     html +=            '<img src="' + img +'" alt="" class="twit__image">'
+//     html +=            <div class="profile_name">
+//                     <h2>nombre</h2>
+//                     let html +=            </div>
+//                 <p class="twit__text">
+//                     texto
+//                 </p>
+//                 <hr>
+//                 <div class="twit__like twit__like--red">
+//                     <i class="fas fa-heart"></i>
+//                 </div>
+//                 <div class="like__result">
+//                 </div>
+//                 <button class="twit__delete">
+//                     Delete
+//                 </button>
+//             </li>
+
+//     return html;
+// }
